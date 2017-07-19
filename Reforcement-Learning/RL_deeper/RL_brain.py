554234c46da9b9c9135bcced437f74fa6b93aba2
lionkt <alien6608@outlook.com>
2017-07-19 22:06:11 +0800

    Start coding pandas-based RL super-class. 1.开始coding基于pandas的面向Q-learning和Sarsa的RL超类，凡是以pandas来存储q-table的RL算法都可以从这个类继承. 2.参考了Morvan的写法. 3.添加了Morvan的Maze环境，便于测试.

---
+++ Reforcement-Learning/RL_deeper/RL_brain.py
@@ -0,0 +1,31 @@
+# 模仿Morvan的例子写的RL的超类，凡是用pandas存储q-table的都可以继承这个类
+
+import numpy as np
+import pandas as pd
+
+
+class RL(object):
+    def __init__(self, actions, learning_rate, gamma, epsilon_greedy):
+        self.actions = actions
+        self.learning_rate = learning_rate
+        self.gamma = gamma
+        self.epsilon_greedy = epsilon_greedy
+        self.q_table = pd.DataFrame(columns=actions)
+
+    def choose_action(self, observation):
+        self.check_state_exist(observation)
+        if np.random.uniform() > self.epsilon_greedy:
+            action_name = np.random.choice(self.actions)
+        else:
+            # 为了应对’动作不同，Q值相同’的情况，这里引入了随机打乱index的操作
+            state_actions = self.q_table.ix[observation, :]
+            state_actions = state_actions.reindex(np.random.permutation(state_actions.index))
+            action_name = state_actions.argmax()
+        return action_name
+
+    # 由于q-table需要动态的扩展，所以在exploration前检查该state是否存在. 如果不存在要添上
+    def check_state_exist(self, observation):
+        ''
+
+    def learn(self):
+        ''
