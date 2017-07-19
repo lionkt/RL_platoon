f3a025aa452ef6775447688e9894755b4e4aa8d8
lionkt <alien6608@outlook.com>
2017-07-19 19:15:49 +0800

    Coding 1D q-learning problem.

--- Reforcement-Learning/RL_deeper/Deeper_RL.py
+++ Reforcement-Learning/RL_deeper/Deeper_RL.py
@@ -14,12 +14,41 @@ def init_q_table(n_states, actions):
     table = pd.DataFrame(np.zeros(n_states, len(actions)), columns=actions)
     return table
 
-def choose_action(s):
 
+def choose_action(state, q_table):
+    '''采用epsilon-greedy策略选择动作'''
+    state_actions = q_table.iloc[state, :]
+    if np.random.uniform() > EPSILON or state_actions.all() == 0:
+        action_name = np.random.choice(ACTIONS)
+    else:
+        action_name = state_actions.argmax()
+    return action_name
+
+
+def get_reward(state, action):
+    if action == 'left':  # move left
+        r = 0
+        if state == 0:
+            s_ = 0
+        else:
+            s_ = state - 1
+    else:  # move right
+        r = 0
+        s_ = state + 1
+        if state == N_STATES - 2:
+            r = 1
+        elif state == N_STATES - 1:
+            s_ = 'terminal'
+    return s_, r
 
 
 def rl():
     q_table = init_q_table(N_STATES, ACTIONS)
     for episode_i in range(MAX_EPISODES):
-        s=0
-
+        s = 0
+        is_end = False
+        while not is_end:
+            a = choose_action(s, q_table)
+            s_, r = get_reward(s, a)
+            Q = q_table.ix[s, a]
+            # TODO 环境的更新
