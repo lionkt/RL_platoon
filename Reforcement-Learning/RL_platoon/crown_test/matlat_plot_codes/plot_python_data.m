c58d5476599d9fb1b6ce2378f52554bc23899aa9
lionkt <alien6608@outlook.com>
2017-10-31 04:12:17 +0800

    Modify matlab plot function and test.

--- Reforcement-Learning/RL_platoon/crown_test/matlat_plot_codes/plot_python_data.m
+++ Reforcement-Learning/RL_platoon/crown_test/matlat_plot_codes/plot_python_data.m
@@ -23,7 +23,8 @@ START_LEADER_TEST_DISTANCE = ROAD_LENGTH / 2;
 %% plot data
 
 disp('开始测试后的误差');
-disp('location误差为:');
+disp('======location误差为:');
+sumup =  0;
 ixx = find(location_data(:,1)>=START_LEADER_TEST_DISTANCE);
 ixx1 = ixx;
 if max(ixx1)>size(inter_dist_data,1)
@@ -32,10 +33,15 @@ end
 for i=1:size(inter_dist_data,2);
     temp_err = inter_dist_data(ixx1,i) - ones(length(inter_dist_data(ixx1,i)),1)*(Desired_inter_distance);
     temp_rmse = sqrt(mean(temp_err.^2));
-    disp(['第',num2str(i),'车和前车的location RSME为：',num2str(temp_rmse),'m']);
+    if i>1
+        sumup = sumup+temp_rmse;
+    end
+    disp(['第',num2str(i),'F车和前车的location RSME为：',num2str(temp_rmse),'m']);
 end
+disp(['从第2辆F车到第',num2str(size(inter_dist_data,2)),'的location RMSE的均值为:',num2str(sumup/(size(inter_dist_data,2)-1))]);
 
-disp('速度误差为:');
+disp('======速度误差为:');
+sumup =  0;
 ixx2 = ixx;
 if max(ixx2)>size(speed_data,1)
     ixx2(ixx2>size(speed_data,1)) = [];
@@ -44,44 +50,61 @@ leader_test_speed = speed_data(ixx2,1);
 for i=2:size(location_data,2);
     temp_err = speed_data(ixx2,i)-leader_test_speed;
     temp_rmse = sqrt(mean(temp_err.^2));
-    disp(['第',num2str(i),'车和前车的speed RSME为：',num2str(temp_rmse),'m/s']);
+    if i>1
+        sumup = sumup+temp_rmse;
+    end
+    disp(['第',num2str(i),'F车和前车的speed RSME为：',num2str(temp_rmse),'m/s']);
 end
-
+disp(['从第2辆F车到第',num2str(size(inter_dist_data,2)),'的speed RMSE的均值为:',num2str(sumup/(size(inter_dist_data,2)-1))]);
 
 
 figure;
-suptitle('dynamics');
-subplot(311);
+% suptitle('dynamics');
+subplot(211);
 time1 = [1:1:size(speed_data,1)]*Time_Step;
+max_speed_arr = ones(size(speed_data,1),1)*MAX_V;
+plot(time1,max_speed_arr,':r','linewidth',1.7);
+hold on;
 for i=1:size(speed_data,2)
     plot(time1, speed_data(:,i),'linewidth',1.7);
     hold on;
 end
+title('velocity');
+ylabel('m/s');
+% set(gca,'xticklabel',[]);    %隐藏x轴
 grid on;
-subplot(312);
+
+subplot(212);
 time2 = [1:1:size(acc_data,1)]*Time_Step;
 for i=1:size(acc_data,2)
     plot(time1, acc_data(:,i),'linewidth',1.7);
     hold on;
 end
+title('acceleration');
+xlabel('time stam(s)');ylabel('m/s^2');
 grid on;
-subplot(313);
-time3 = [1:1:size(jerk_data,1)]*Time_Step;
-for i=1:size(jerk_data,2)
-    plot(time3, jerk_data(:,i),'linewidth',1.7);
-    hold on;
-end
-grid on;
+% subplot(313);
+% time3 = [1:1:size(jerk_data,1)]*Time_Step;
+% for i=1:size(jerk_data,2)
+%     plot(time3, jerk_data(:,i),'linewidth',1.7);
+%     hold on;
+% end
+% grid on;
 
 
 figure;
-suptitle('location');
+% suptitle('location');
 subplot(211);
 time1 = [1:1:size(inter_dist_data,1)]*Time_Step;
+des_inter_dist = ones(size(inter_dist_data,1),1)*Desired_inter_distance;
+plot(time1,des_inter_dist,':r','linewidth',1.7);
+hold on;
 for i=1:size(inter_dist_data,2)
     plot(time1, inter_dist_data(:,i),'linewidth',1.7);
     hold on;
 end
+title('Inter-vehicle Spacing');
+ylabel('m');xlabel('time stamp (s)');
 grid on;
 subplot(212);
 for i=1:size(inter_dist_data,2)-1
@@ -93,6 +116,8 @@ for i=1:size(inter_dist_data,2)-1
     hold on;
 end
 grid on;
+title('Frequency Domain Error Ratio');
+ylabel('amplitude');xlabel('frequency');
 
 
 
